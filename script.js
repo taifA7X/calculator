1841d4f501abed29cca841b8526a5cffa8053ee3
Taif Ahmad <a7x.taif@gmail.com>
2021-10-06 20:07:27 +0530

    add button events

--- script.js
+++ script.js
@@ -23,3 +23,30 @@ function operate(operator, a, b) {
     return result
 }
 
+const display = document.querySelector('#display')
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+const zero = document.querySelector('#zero')
+const decimal = document.querySelector('#decimal')
+const clear = document.querySelector('#clear')
+
+
+one.addEventListener('click', () => display.innerHTML += 1)
+two.addEventListener('click', () => display.innerHTML += 2)
+three.addEventListener('click', () => display.innerHTML += 3)
+four.addEventListener('click', () => display.innerHTML += 4)
+five.addEventListener('click', () => display.innerHTML += 5)
+six.addEventListener('click', () => display.innerHTML += 6)
+seven.addEventListener('click', () => display.innerHTML += 7)
+eight.addEventListener('click', () => display.innerHTML += 8)
+nine.addEventListener('click', () => display.innerHTML += 9)
+zero.addEventListener('click', () => display.innerHTML += 0)
+decimal.addEventListener('click', () => display.innerHTML += '.')
+clear.addEventListener('click', () => display.innerHTML = '')
