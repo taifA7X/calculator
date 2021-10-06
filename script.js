6af1418c79d005a99c7369b73c6c0cab0b6279db
Taif Ahmad <a7x.taif@gmail.com>
2021-10-06 14:34:00 +0530

    made ui

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+    let result = a + b;
+    return result
+}
+
+function subtract(a, b) {
+    let result = a - b;
+    return result
+}
+
+function multiply(a, b) {
+    let result = a * b;
+    return result
+}
+
+function divide(a, b) {
+    result = a / b;
+    return result
+}
+
+function operate(operator, a, b) {
+    result = operator(a, b);
+    return result
+}
+
